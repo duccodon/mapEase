bc6f4a27961933703c31d8270eaa2f4369a4ea7e
TonyTran2902a <tranbuu2902@gmail.com>
2025-03-08 11:59:14 +0700

    add functions: type changed, zoom, compass

--- app/src/main/java/com/example/mapease/MainActivity.java
+++ app/src/main/java/com/example/mapease/MainActivity.java
@@ -2,13 +2,11 @@ package com.example.mapease;
 
 import android.location.Location;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -52,5 +50,42 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         LatLng HCM = new LatLng( 10, 106);
         myMap.addMarker(new MarkerOptions().position(HCM).title("HCM city"));
         myMap.moveCamera(CameraUpdateFactory.newLatLng(HCM));
+
+        myMap.getUiSettings().setZoomControlsEnabled(true);
+        myMap.getUiSettings().setCompassEnabled(true);
+
+        myMap.setPadding(0, 220, 0, 0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_map_type, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.mapNone){
+            myMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+        }
+
+        if(id == R.id.mapNormal){
+            myMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+
+        if(id == R.id.mapSatellite){
+            myMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+
+        if(id == R.id.mapHybrid){
+            myMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        }
+
+        if(id == R.id.mapTerrain){
+            myMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
