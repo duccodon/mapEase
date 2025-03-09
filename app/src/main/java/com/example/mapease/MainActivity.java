0659e2531e1ebdb197fa3c1799610bde48ffea7f
yamee4 <yamee4804@gmail.com>
2025-03-09 11:56:27 +0700

    add SearchView

--- app/src/main/java/com/example/mapease/MainActivity.java
+++ app/src/main/java/com/example/mapease/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.mapease;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,10 +19,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private final int FINE_PERMISSION_CODE = 1;
     private GoogleMap myMap;
+
+    private SearchView mapSearchView;
+
     Location currentLocation;
     //FusedLocationProviderClient locationProviderClient;
 
@@ -34,9 +44,42 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             return insets;
         });*/
 
+        mapSearchView = findViewById(R.id.mapSearch);
+
         //locationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+
+
+        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = mapSearchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if(location != null){
+                    Geocoder geocoder = new Geocoder(MainActivity.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    myMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        mapFragment.getMapAsync(MainActivity.this);
     }
 
     /*private void getLastLocation() {
@@ -47,9 +90,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         myMap = googleMap;
 
-        LatLng HCM = new LatLng( 10, 106);
-        myMap.addMarker(new MarkerOptions().position(HCM).title("HCM city"));
-        myMap.moveCamera(CameraUpdateFactory.newLatLng(HCM));
+//        LatLng HCM = new LatLng( 10, 106);
+//        myMap.addMarker(new MarkerOptions().position(HCM).title("HCM city"));
+//        myMap.moveCamera(CameraUpdateFactory.newLatLng(HCM));
 
         myMap.getUiSettings().setZoomControlsEnabled(true);
         myMap.getUiSettings().setCompassEnabled(true);
